b8b0b56f64a009e6c1d9bdb59e7ed6ea5a760a64
shantanug-cuelogic <shantanu.gade@cuelogic.com>
2019-12-19 15:36:48 +0530

    Updates

---
+++ precache-manifest.68aca6d4bb659bbbdd8ac2f4df987729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc737895f869fa8170a50e6ca0c3aec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d225f92d5c11d6ace7b",
+    "url": "/static/css/main.dfa2290e.chunk.css"
+  },
+  {
+    "revision": "3a81284b5c5d804d78a0",
+    "url": "/static/js/2.e34cc853.chunk.js"
+  },
+  {
+    "revision": "5d225f92d5c11d6ace7b",
+    "url": "/static/js/main.46350812.chunk.js"
+  },
+  {
+    "revision": "7d92703e7f2954eebf49",
+    "url": "/static/js/runtime-main.1aa3e77d.js"
+  },
+  {
+    "revision": "646e641765bfde4bf557809752329ecc",
+    "url": "/static/media/earth-dark.646e6417.jpg"
+  },
+  {
+    "revision": "56e5c0687430c9eff705e6ffbd82ce31",
+    "url": "/static/media/earth-topology.56e5c068.png"
+  }
+]);
