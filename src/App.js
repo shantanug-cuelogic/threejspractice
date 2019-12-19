44dc7716bf0935e6f5e535a6e06e5f36044b7b8c
shantanug-cuelogic <shantanu.gade@cuelogic.com>
2019-12-19 16:45:40 +0530

    Added ios promt

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route} from "react-router-dom";
 import './App.css';
 import ViewContainer from "./View";
+import PWAPrompt from 'react-ios-pwa-prompt'
 
 function App() {
   return (
     <div className="App">
-        
+        <PWAPrompt />
+
         <Route path={"/"} component={ViewContainer} ></Route>
     </div>
   );
