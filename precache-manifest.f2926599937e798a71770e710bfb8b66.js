4fdf54baff006e2690ac56534a770afde883daaf
shantanug-cuelogic <shantanu.gade@cuelogic.com>
2019-12-19 15:56:47 +0530

    Updates

---
+++ precache-manifest.f2926599937e798a71770e710bfb8b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc013fea4606ad330053df6063e4899",
+    "url": "/threejspractice/index.html"
+  },
+  {
+    "revision": "eed66810e06af5636e21",
+    "url": "/threejspractice/static/css/main.dfa2290e.chunk.css"
+  },
+  {
+    "revision": "862d5e9bfef33cf25287",
+    "url": "/threejspractice/static/js/2.45b621c4.chunk.js"
+  },
+  {
+    "revision": "eed66810e06af5636e21",
+    "url": "/threejspractice/static/js/main.87d76dcd.chunk.js"
+  },
+  {
+    "revision": "3a843c7cae7d8694e2e2",
+    "url": "/threejspractice/static/js/runtime-main.42d0a2c5.js"
+  },
+  {
+    "revision": "646e641765bfde4bf557809752329ecc",
+    "url": "/threejspractice/static/media/earth-dark.646e6417.jpg"
+  },
+  {
+    "revision": "56e5c0687430c9eff705e6ffbd82ce31",
+    "url": "/threejspractice/static/media/earth-topology.56e5c068.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e81978b6a854b7c48017357ac7a65498.js"
+  "/threejspractice/precache-manifest.f2926599937e798a71770e710bfb8b66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/threejspractice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
